9036b5b326801116a46c9dabb692f86b0771b989
Dhruv Prajapati <dhruvprajapati66572@gmail.com>
2025-07-06 20:13:10 +0530

    Fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
@@ -9,7 +8,6 @@ import ContactUs from '@/components/ContactUs';
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
       <Hero />
       <About />
       <Services />
